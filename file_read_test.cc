1f0ca54abccdc8758a33a88a66e8d7d7b4723a44
Troy Denton <troy@troydenton.ca>
2023-11-18 11:02:40 -0500

    connectivity test

--- file_read_test.cc
+++ file_read_test.cc
@@ -18,3 +18,55 @@ TEST(FileRead, cct1_count_nets) {
     delete c;
 }
 
+TEST(FileRead, cct1_conns) {
+    circuit* c = new circuit("../data/cct1");
+    cell* c1 = c->get_cell("1");
+    cell* c2 = c->get_cell("2");
+    cell* c3 = c->get_cell("3");
+    cell* c4 = c->get_cell("4");
+    cell* c5 = c->get_cell("5");
+    cell* c9 = c->get_cell("9");
+    cell* c11 = c->get_cell("11");
+    cell* c12 = c->get_cell("12");
+
+    // these all share net 26
+    ASSERT_TRUE( c1->is_connected_to(c3) );
+    ASSERT_TRUE( c1->is_connected_to(c4) );
+    ASSERT_TRUE( c1->is_connected_to(c5) );
+    ASSERT_TRUE( c1->is_connected_to(c9) );
+    ASSERT_TRUE( c1->is_connected_to(c11) );
+
+    ASSERT_TRUE( c3->is_connected_to(c1) );
+    ASSERT_TRUE( c3->is_connected_to(c4) );
+    ASSERT_TRUE( c3->is_connected_to(c5) );
+    ASSERT_TRUE( c3->is_connected_to(c9) );
+    ASSERT_TRUE( c3->is_connected_to(c11) );
+
+    ASSERT_TRUE( c4->is_connected_to(c1) );
+    ASSERT_TRUE( c4->is_connected_to(c3) );
+    ASSERT_TRUE( c4->is_connected_to(c5) );
+    ASSERT_TRUE( c4->is_connected_to(c9) );
+    ASSERT_TRUE( c4->is_connected_to(c11) );
+
+    ASSERT_TRUE( c5->is_connected_to(c1) );
+    ASSERT_TRUE( c5->is_connected_to(c3) );
+    ASSERT_TRUE( c5->is_connected_to(c4) );
+    ASSERT_TRUE( c5->is_connected_to(c9) );
+    ASSERT_TRUE( c5->is_connected_to(c11) );
+
+    ASSERT_TRUE( c9->is_connected_to(c1) );
+    ASSERT_TRUE( c9->is_connected_to(c3) );
+    ASSERT_TRUE( c9->is_connected_to(c4) );
+    ASSERT_TRUE( c9->is_connected_to(c5) );
+    ASSERT_TRUE( c9->is_connected_to(c11) );
+
+    ASSERT_TRUE( c11->is_connected_to(c1) );
+    ASSERT_TRUE( c11->is_connected_to(c3) );
+    ASSERT_TRUE( c11->is_connected_to(c4) );
+    ASSERT_TRUE( c11->is_connected_to(c5) );
+    ASSERT_TRUE( c11->is_connected_to(c9) );
+
+    // cell 2 is not on net 26
+    ASSERT_FALSE( c2->is_connected_to(c12) );
+    delete c;
+}
